025180fca1bca42bdbff86ccb236b32f8f270bcf
Krishnesha <krishneshasarma@gmail.com>
2026-04-29 20:16:29 +0530

    program18.cpp

---
+++ program18.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Parent
+{
+    public:
+    void display()
+    {
+        cout<<"Parent"<<endl;
+    }
+};
+
+class Child1 : public Parent
+{
+};
+
+class Child2 : public Parent
+{
+};
+
+int main()
+{
+    Child1 c1;
+    Child2 c2;
+
+    c1.display();
+    c2.display();
+}
